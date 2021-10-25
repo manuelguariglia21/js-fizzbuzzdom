621dbcaeb768a8dd43dee0dd14296ddd3ce90386
Manuel Guariglia <manuelguariglia@gmail.com>
2021-10-25 16:21:18 +0200

    esercizio terminato

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 const row = document.querySelector('.row');
 
 for(let i = 1; i < 101; i++){
-  const box = document.createElement('div'); //crea un elemento
-  box.className = 'box'; //assegna a un elemento una classe
-  box.innerHTML= i ; //aggiunge il contenuto all'interno del elemento
-  row.append(box); //aggiunge un elemento
+  const box = document.createElement('div');
+  box.className = 'box'; 
+  box.innerHTML= i ; 
+  row.append(box);
+
+  if(!(i%3) && !(i%5)){
+    box.classList.add('fizzBuzz');
+    box.innerHTML = "fizzbuzz" ;
+  }
+
+  else if(!(i%3)){
+    box.classList.add('fizz');
+    box.innerHTML = "fizz" ;
+  }
+
+  else if(!(i%5)){
+    box.classList.add('buzz');
+    box.innerHTML = "buzz" ;
+  }
 
 }
